cab962f30f3f4bf5b4c13d6ba39d07f1fd27fb91
Rich Jenks <rich@richjenks.com>
2014-11-18 15:24:51 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-wp-nf-post-form-name
-====================
+# Post Form Name
 
-Adds the hidden field _form_name to all forms
+Adds the hidden field `_form_name` to all forms
+
+As per http://ninjaforms.com/documentation/developer-api/code-examples/using-extra-field-values/ it is ignored by Ninja Forms (so you're not storing the form name in each form submission) but it's still available to developers and other plugins.
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: Post Form Name
+ * Plugin URI: https://github.com/richjenks/wp-nf-post-form-name
+ * Description: Adds the hidden field _form_name to all forms
+ * Version: 1.0
+ * Author: Rich Jenks <rich@richjenks.com>
+ * Author URI: http://richjenks.com
+ * License: GPL2
+ */
+
+add_action( 'ninja_forms_display_after_fields', function() {
+	global $ninja_forms_loading;
+	$form_name = $ninja_forms_loading->get_form_setting( 'form_title' );
+	echo '<input type="hidden" name="_form_name" value="' . $form_name . '">';
+} );
