35825754abba0b7151ea718c205f221f55f88a35
Rich Jenks <rich@richjenks.com>
2014-11-24 09:55:49 +0000

    Moved reference comments

--- inc/Fields.php
+++ inc/Fields.php
@@ -7,3 +7,26 @@
  */
 
 namespace RichJenks\NFRecon;
+
+// add_action( 'admin_menu', function() {
+// 	global $menu;
+// 	var_dump( $menu );
+// }, 9999 );
+
+// add_action( 'ninja_forms_display_pre_init', function() {
+
+// 	global $ninja_forms_loading;
+// 	global $ninja_forms_processing;
+
+// 	// Either form is being displayed or success page is
+// 	if ( isset( $ninja_forms_loading ) ) {
+// 		$form_name = $ninja_forms_loading->data['form']['form_title'];
+// 	} elseif ( isset( $ninja_forms_processing ) ) {
+// 		$form_name = $ninja_forms_processing->data['form']['form_title'];
+// 	}
+
+// 	add_action( 'ninja_forms_display_fields', function() use ( $form_name ) {
+// 		echo '<input type="hidden" name="_form_name" value="' . $form_name . '">';
+// 	} );
+
+// } );
--- index.php
+++ index.php
@@ -20,26 +20,3 @@ require_once 'inc/Fields.php';
 
 // Add admin page if back-end
 if ( is_admin() ) new Admin;
-
-// add_action( 'admin_menu', function() {
-// 	global $menu;
-// 	var_dump( $menu );
-// }, 9999 );
-
-// add_action( 'ninja_forms_display_pre_init', function() {
-
-// 	global $ninja_forms_loading;
-// 	global $ninja_forms_processing;
-
-// 	// Either form is being displayed or success page is
-// 	if ( isset( $ninja_forms_loading ) ) {
-// 		$form_name = $ninja_forms_loading->data['form']['form_title'];
-// 	} elseif ( isset( $ninja_forms_processing ) ) {
-// 		$form_name = $ninja_forms_processing->data['form']['form_title'];
-// 	}
-
-// 	add_action( 'ninja_forms_display_fields', function() use ( $form_name ) {
-// 		echo '<input type="hidden" name="_form_name" value="' . $form_name . '">';
-// 	} );
-
-// } );
